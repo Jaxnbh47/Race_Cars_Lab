cede122053dbea91ddbfaa67e7d57d9b6b1e6035
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-12-19 20:41:04 -0500

    master

--- interfaces/IRacer.ts
+++ interfaces/IRacer.ts
@@ -5,35 +5,3 @@ interface Racer {
   isFuelEmpty(): boolean;
 }
 export { Racer }
-
-// function findRacersWithEmptyFuel(racers: Racer[]){
-//   return racers.filter((racer) => racer.isFuelEmpty());
-// };
-
-// //function findRacersWithEmptyFuel(racers : Racer[]){
-// // let array = [];
-// // for(let i = 0; i<array.length; i++){
-// //  racers[i].isFuelEmpty()
-// // }
-// // }
-
-// function findAverageSpeed(racers: Racer[]){
-//   let totalSpeed = 0;
-//   for (const racer of racers) {
-//     totalSpeed += racer.speed;
-//         const averageSpeed = totalSpeed / racers.length;
-//   return averageSpeed;
-//   }
-//   const averageSpeed = totalSpeed / racers.length;
-//   return averageSpeed;
-// };
-
-// function getFasterRacer(racerA: Racer, racerB: Racer): Racer | null {
-//   if (racerA.speed > racerB.speed) {
-//     return racerA;
-//   } else if (racerB.speed > racerA.speed) {
-//     return racerB;
-//   } else {
-//     return null;
-//   };
-// };
